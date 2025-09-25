96aa4ba47846579c5d84af66284700c92c9621d9
Karan Raj <ks305842@gmail.com>
2025-09-25 05:54:27 +0530

    vite changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      // Proxy API requests to backend in development
-      '/api': {
-        target: 'http://localhost:5000', // your local backend port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: 'build', // match Vercel static build folder
-  },
-});
+  plugins: [react(), tailwindcss()],
+})
